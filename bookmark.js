060c9a10c50f9cfe5e618157dc6d1b310453ee0c
Joseph Henry Ilagan <josephhenryilagan@gmail.com>
2026-04-02 16:43:07 +0800

    feature: added bookmark implementation

---
+++ bookmark.js
@@ -0,0 +1,6 @@
+javascript: (function () {
+    const script = document.createElement('script');
+    script.src = 'https://cdn.jsdelivr.net/gh/josephhenryilagan/servicenow-toolkit-lite@main/ServiceNow%20Toolkit.js';
+    script.type = 'text/javascript';
+    document.head.appendChild(script);
+})();
